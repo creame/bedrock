4ac7a650e0269e98d061fedc43eff30891415a45
Paco Toledo <pacotole@crea.me>
2018-06-08 12:16:09 +0200

    [IMP] Development disable plugins

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,11 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+/** Disabled publins on development */
+define('DISABLED_PLUGINS', serialize([
+  'autoptimize/autoptimize.php',
+  'simple-cache/simple-cache.php',
+  'ithemes-security-pro/ithemes-security-pro.php',
+  'google-analytics-dashboard-for-wp/gadwp.php'
+]));
---
+++ web/app/mu-plugins/environment-plugin-disabler.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Plugin Name: Environment Plugin Disabler
+ * Description: Disables plugins based on environment settings.
+ * Version:     1.0.0
+ * Author:      Creame
+ * Author URI:  https://crea.me/
+ *
+ *
+ * Usage: define a DISABLED_PLUGINS constant with a serialized array
+ * of plugins to disable (e.g.):
+ *
+ * define('DISABLED_PLUGINS', serialize([
+ *    'autoptimize/autoptimize.php',
+ *    'wp-super-cache/wp-cache.php'
+ * ]));
+ *
+ * Based on post https://roots.io/guides/how-to-disable-plugins-on-certain-environments/
+ *
+ */
+
+
+
+/**
+ * Class DisablePlugins
+ * Author: Mark Jaquith
+ * Author URI: http://markjaquith.com/
+ * Class  URI: https://gist.github.com/markjaquith/1044546
+ * Using fork: https://gist.github.com/Rarst/4402927
+ */
+
+class DisablePlugins
+{
+    public static $instance;
+    private $disabled = [];
+
+    /**
+     * Sets up the options filter, and optionally handles an array of plugins to disable
+     * @param array $disables Optional array of plugin filenames to disable
+     */
+    public function __construct(array $disables = null)
+    {
+        /**
+         * Handle what was passed in
+         */
+        if (is_array($disables)) {
+            foreach ($disables as $disable) {
+                $this->disable($disable);
+            }
+        }
+
+        /**
+         * Add the filters
+         */
+        add_filter('option_active_plugins', [$this, 'do_disabling']);
+        add_filter('site_option_active_sitewide_plugins', [$this, 'do_network_disabling']);
+
+        /**
+         * Allow other plugins to access this instance
+         */
+        self::$instance = $this;
+    }
+
+    /**
+     * Adds a filename to the list of plugins to disable
+     */
+    public function disable($file)
+    {
+        $this->disabled[] = $file;
+    }
+
+    /**
+     * Hooks in to the option_active_plugins filter and does the disabling
+     * @param array $plugins WP-provided list of plugin filenames
+     * @return array The filtered array of plugin filenames
+     */
+    public function do_disabling($plugins)
+    {
+        if (count($this->disabled)) {
+            foreach ((array)$this->disabled as $plugin) {
+                $key = array_search($plugin, $plugins);
+                if (false !== $key) {
+                    unset($plugins[$key]);
+                }
+            }
+        }
+
+        return $plugins;
+    }
+
+    /**
+     * Hooks in to the site_option_active_sitewide_plugins filter and does the disabling
+     *
+     * @param array $plugins
+     *
+     * @return array
+     */
+    public function do_network_disabling($plugins)
+    {
+        if (count($this->disabled)) {
+            foreach ((array)$this->disabled as $plugin) {
+                if (isset($plugins[$plugin])) {
+                    unset($plugins[$plugin]);
+                }
+            }
+        }
+
+        return $plugins;
+    }
+}
+
+
+/* Begin plugin disable initialization */
+
+if (defined('DISABLED_PLUGINS')) {
+    $plugins_to_disable = unserialize(DISABLED_PLUGINS);
+
+    if (!empty($plugins_to_disable) && is_array($plugins_to_disable)) {
+        $utility = new DisablePlugins($plugins_to_disable);
+
+        //part below is optional but for me it is crucial
+        error_log('Environment disabled plugins: ' . var_export($plugins_to_disable, true));
+    }
+}
