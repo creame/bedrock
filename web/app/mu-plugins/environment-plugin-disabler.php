75bb6022df42bcb2167e14cd0c24bf4888f8f7ea
Paco Toledo <pacotole@crea.me>
2019-03-01 12:10:31 +0100

    Updated plugin disabler

--- web/app/mu-plugins/environment-plugin-disabler.php
+++ web/app/mu-plugins/environment-plugin-disabler.php
@@ -6,8 +6,7 @@
  * Author:      Creame
  * Author URI:  https://crea.me/
  *
- *
- * Usage: define a DISABLED_PLUGINS constant with a serialized array
+ * Usage: define and DISABLED_PLUGINS constant with a serialized array
  * of plugins to disable (e.g.):
  *
  * define('DISABLED_PLUGINS', serialize([
@@ -16,7 +15,6 @@
  * ]));
  *
  * Based on post https://roots.io/guides/how-to-disable-plugins-on-certain-environments/
- *
  */
 
 
@@ -29,96 +27,99 @@
  * Using fork: https://gist.github.com/Rarst/4402927
  */
 
-class DisablePlugins
-{
-    public static $instance;
-    private $disabled = [];
+class DisablePlugins {
 
-    /**
-     * Sets up the options filter, and optionally handles an array of plugins to disable
-     * @param array $disables Optional array of plugin filenames to disable
-     */
-    public function __construct(array $disables = null)
-    {
-        /**
-         * Handle what was passed in
-         */
-        if (is_array($disables)) {
-            foreach ($disables as $disable) {
-                $this->disable($disable);
-            }
-        }
+	public static $instance;
+	private $disabled = [];
 
-        /**
-         * Add the filters
-         */
-        add_filter('option_active_plugins', [$this, 'do_disabling']);
-        add_filter('site_option_active_sitewide_plugins', [$this, 'do_network_disabling']);
+	/**
+	 * Sets up the options filter, and optionally handles an array of plugins to disable
+	 *
+	 * @param array $disables Optional array of plugin filenames to disable
+	 */
+	public function __construct( array $disables = null ) {
+		/**
+		 * Handle what was passed in
+		 */
+		if ( is_array( $disables ) ) {
+			foreach ( $disables as $disable ) {
+				$this->disable( $disable );
+			}
+		}
 
-        /**
-         * Allow other plugins to access this instance
-         */
-        self::$instance = $this;
-    }
+		/**
+		 * Add the filters
+		 */
+		add_filter( 'option_active_plugins', [ $this, 'do_disabling' ] );
+		add_filter( 'site_option_active_sitewide_plugins', [ $this, 'do_network_disabling' ] );
 
-    /**
-     * Adds a filename to the list of plugins to disable
-     */
-    public function disable($file)
-    {
-        $this->disabled[] = $file;
-    }
+		/**
+		 * Allow other plugins to access this instance
+		 */
+		self::$instance = $this;
+	}
 
-    /**
-     * Hooks in to the option_active_plugins filter and does the disabling
-     * @param array $plugins WP-provided list of plugin filenames
-     * @return array The filtered array of plugin filenames
-     */
-    public function do_disabling($plugins)
-    {
-        if (count($this->disabled)) {
-            foreach ((array)$this->disabled as $plugin) {
-                $key = array_search($plugin, $plugins);
-                if (false !== $key) {
-                    unset($plugins[$key]);
-                }
-            }
-        }
+	/**
+	 * Adds a filename to the list of plugins to disable
+	 */
+	public function disable( $file ) {
+		$this->disabled[] = $file;
+	}
 
-        return $plugins;
-    }
+	/**
+	 * Hooks in to the option_active_plugins filter and does the disabling
+	 *
+	 * @param array $plugins WP-provided list of plugin filenames
+	 * @return array The filtered array of plugin filenames
+	 */
+	public function do_disabling( $plugins ) {
+		if ( count( $this->disabled ) ) {
+			foreach ( (array) $this->disabled as $plugin ) {
+				$key = array_search( $plugin, $plugins );
+				if ( false !== $key ) {
+					unset( $plugins[ $key ] );
+				}
+			}
+		}
 
-    /**
-     * Hooks in to the site_option_active_sitewide_plugins filter and does the disabling
-     *
-     * @param array $plugins
-     *
-     * @return array
-     */
-    public function do_network_disabling($plugins)
-    {
-        if (count($this->disabled)) {
-            foreach ((array)$this->disabled as $plugin) {
-                if (isset($plugins[$plugin])) {
-                    unset($plugins[$plugin]);
-                }
-            }
-        }
+		return $plugins;
+	}
 
-        return $plugins;
-    }
+	/**
+	 * Hooks in to the site_option_active_sitewide_plugins filter and does the disabling
+	 *
+	 * @param array $plugins
+	 *
+	 * @return array
+	 */
+	public function do_network_disabling( $plugins ) {
+		if ( count( $this->disabled ) ) {
+			foreach ( (array) $this->disabled as $plugin ) {
+				if ( isset( $plugins[ $plugin ] ) ) {
+					unset( $plugins[ $plugin ] );
+				}
+			}
+		}
+
+		return $plugins;
+	}
 }
 
 
 /* Begin plugin disable initialization */
 
-if (defined('DISABLED_PLUGINS')) {
-    $plugins_to_disable = unserialize(DISABLED_PLUGINS);
+if ( defined( 'DISABLED_PLUGINS' ) ) {
+	$plugins_to_disable = unserialize( DISABLED_PLUGINS );
 
-    if (!empty($plugins_to_disable) && is_array($plugins_to_disable)) {
-        $utility = new DisablePlugins($plugins_to_disable);
+	if ( ! empty( $plugins_to_disable ) && is_array( $plugins_to_disable ) ) {
+		$utility = new DisablePlugins( $plugins_to_disable );
 
-        //part below is optional but for me it is crucial
-        error_log('Environment disabled plugins: ' . var_export($plugins_to_disable, true));
-    }
+		// part below is optional but for me it is crucial
+		$info = array_map(
+			function( $item ) {
+				return explode( '/', $item )[0];
+			}, $plugins_to_disable
+		);
+		error_log( 'Environment disabled plugins: ' . implode( ', ', $info ) );
+	}
 }
