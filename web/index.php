1b035779ed06eef471b2ae90ad61508afc264e89
Paco Toledo <pacotole@crea.me>
2019-03-01 11:07:30 +0100

    Ensure "current" ABSPATH

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 
 /** WordPress view bootstrapper */
+define('ABSPATH', preg_replace('/\/releases\/\d+\//', '/current/', dirname(__FILE__)) . '/wp/');
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
