f402cd72efa33cc3c4be2976d97e989eadbb6f25
Paco Toledo <pacotole@crea.me>
2022-04-05 18:18:52 +0200

    Set post revisions via env

--- config/application.php
+++ config/application.php
@@ -125,7 +125,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
  * Creame Settings
  */
 Config::define('WPLANG', 'es_ES');
-Config::define('WP_POST_REVISIONS', 5);
 Config::define('WP_MEMORY_LIMIT', '256M');
 Config::define('WP_CACHE', true);
 Config::define('WP_CACHE_KEY_SALT', 'example');
