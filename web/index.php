8e98ecede8735e63fe4c84232f278132c9a74bb8
Paco Toledo <pacotole@crea.me>
2022-10-03 10:24:42 +0200

    index use __DIR__

--- web/index.php
+++ web/index.php
@@ -2,6 +2,6 @@
 /**
  * WordPress View Bootstrapper
  */
-define('ABSPATH', preg_replace('/\/releases\/\d+\//', '/current/', dirname(__FILE__)) . '/wp/');
+define('ABSPATH', preg_replace('/\/releases\/\d+\//', '/current/', __DIR__) . '/wp/');
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
